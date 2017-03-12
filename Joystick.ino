5a7e9a795f58a0c7e97df1a8c779c76f43e1b7ce
drzzm32 <drzzm32@gmail.com>
2017-03-12 09:51:18 +0800

    init

--- Joystick.ino
+++ Joystick.ino
@@ -1,129 +1,106 @@
 #include "Keyboard.h"
 
-#define KEY_LUP      6
-#define KEY_LLEFT    8
-#define KEY_LRIGHT   9
-#define KEY_L1       5
-#define KEY_L2       7
+enum Key {
+    LWheelA = 2,
+    LWheelB = 3,
+    RWheelA = 0,
+    RWheelB = 1,
+    LWheelBtn = A2,
+    RWheelBtn = A3,
+    CLed = 4,
+    LLedR = 5,
+    LLedY = 6,
+    LLedG = 7,
+    RLedR = 8,
+    RLedY = 9,
+    RLedG = 10,
+    SwitchA = 16,
+    SwitchB = 14,
+    SwitchC = 15,
+    SwitchD = A0,
+    SwitchE = A1,
+};
 
-#define KEY_RUP      15
-#define KEY_RLEFT    10
-#define KEY_RRIGHT   16
-#define KEY_R1       14
-#define KEY_R2       A0
+#define INPORTS_CNT 11
+uint8_t inPorts[] = {
+    LWheelA, LWheelB,
+    RWheelA, RWheelB,
+    LWheelBtn, RWheelBtn,
+    SwitchA, SwitchB, SwitchC, SwitchD, SwitchE
+};
+#define OUTPORTS_CNT 7
+uint8_t outPorts[] = {
+    CLed,
+    LLedR, LLedY, LLedG,
+    RLedR, RLedY, RLedG
+};
 
-#define MODE_A       0
-#define MODE_B       1
+bool stateLA = false, stateLB = false;
+bool stateRA = false, stateRB = false;
+
+void INT_LWheelA() {
+    stateLA = true;
+    if (stateLB) {
+        // Clock
+        Keyboard.write('s');
+        stateLB = false;
+        stateLA = false;
+    }
+}
+
+void INT_LWheelB() {
+    stateLB = true;
+    if (stateLA) {
+        // Anti-Clock
+        Keyboard.write('w');
+        stateLA = false;
+        stateLB = false;
+    }
+}
+
+void INT_RWheelA() {
+    stateRA = true;
+    if (stateRB) {
+        // Clock
+        Keyboard.write('d');
+        stateRB = false;
+        stateRA = false;
+    }
+}
+
+void INT_RWheelB() {
+    stateRB = true;
+    if (stateRA) {
+        // Anti-Clock
+        Keyboard.write('a');
+        stateRA = false;
+        stateRB = false;
+    }
+}
 
 void setup() {
-    /*INPUT Mode*/
-    pinMode(KEY_LUP, INPUT);
-    pinMode(KEY_LLEFT, INPUT);
-    pinMode(KEY_LRIGHT, INPUT);
-    pinMode(KEY_L1, INPUT);
-    pinMode(KEY_L2, INPUT);
+    /*OUTPUT Mode*/
+    for (uint8_t i = 0; i < OUTPORTS_CNT; i++)
+        pinMode(outPorts[i], OUTPUT);
     
-    pinMode(KEY_RUP, INPUT);
-    pinMode(KEY_RLEFT, INPUT);
-    pinMode(KEY_RRIGHT, INPUT);
-    pinMode(KEY_R1, INPUT);
-    pinMode(KEY_R2, INPUT);
-
-    pinMode(MODE_A, INPUT);
-    pinMode(MODE_B, INPUT);
-    
-    /*PULL UP*/
-    digitalWrite(KEY_LUP, HIGH);
-    digitalWrite(KEY_LLEFT, HIGH);
-    digitalWrite(KEY_LRIGHT, HIGH);
-    digitalWrite(KEY_L1, HIGH);
-    digitalWrite(KEY_L2, HIGH);
-    
-    digitalWrite(KEY_RUP, HIGH);
-    digitalWrite(KEY_RLEFT, HIGH);
-    digitalWrite(KEY_RRIGHT, HIGH);
-    digitalWrite(KEY_R1, HIGH);
-    digitalWrite(KEY_R2, HIGH);
-
-    digitalWrite(MODE_A, HIGH);
-    digitalWrite(MODE_B, HIGH);
+    /*INPUT Mode and PULLUP*/
+    for (uint8_t i = 0; i < INPORTS_CNT; i++)
+        pinMode(inPorts[i], INPUT_PULLUP);
 
     delay(1000);
     
     Keyboard.begin();
+
+    /*Attach Interrupt*/
+    
+    attachInterrupt(digitalPinToInterrupt(LWheelA), &INT_LWheelA, FALLING);
+    attachInterrupt(digitalPinToInterrupt(LWheelB), &INT_LWheelB, FALLING);
+    attachInterrupt(digitalPinToInterrupt(RWheelA), &INT_RWheelA, FALLING);
+    attachInterrupt(digitalPinToInterrupt(RWheelB), &INT_RWheelB, FALLING);
+    
 }
 
 void loop() {
-    if (!digitalRead(MODE_A) && digitalRead(MODE_B)) {
-
-        if (digitalRead(KEY_L1)) Keyboard.release('s');
-        else Keyboard.press('s');
-        if (digitalRead(KEY_LUP)) Keyboard.release('d');
-        else Keyboard.press('d');  
-        if (digitalRead(KEY_L2)) Keyboard.release('f');
-        else Keyboard.press('f');
-        if (digitalRead(KEY_R1)) Keyboard.release('j');
-        else Keyboard.press('j');
-        if (digitalRead(KEY_RUP)) Keyboard.release('k');
-        else Keyboard.press('k');
-        if (digitalRead(KEY_R2)) Keyboard.release('l');
-        else Keyboard.press('l');
-        
-        if (digitalRead(KEY_LRIGHT) || digitalRead(KEY_RLEFT)) Keyboard.release(KEY_ESC);
-        else Keyboard.press(KEY_ESC);
-        if (digitalRead(KEY_LLEFT) || digitalRead(KEY_RRIGHT)) Keyboard.release('`');
-        else Keyboard.press('`');
-        
-    } else if (!digitalRead(MODE_B) && digitalRead(MODE_A)) {
-        
-        if (digitalRead(KEY_RUP)) Keyboard.release(KEY_UP_ARROW);
-        else Keyboard.press(KEY_UP_ARROW);
-        if (digitalRead(KEY_RLEFT)) Keyboard.release(KEY_LEFT_ARROW);
-        else Keyboard.press(KEY_LEFT_ARROW);  
-        if (digitalRead(KEY_RRIGHT)) Keyboard.release(KEY_RIGHT_ARROW);
-        else Keyboard.press(KEY_RIGHT_ARROW);
-        if (digitalRead(KEY_LUP)) Keyboard.release(KEY_DOWN_ARROW);
-        else Keyboard.press(KEY_DOWN_ARROW);
-        if (digitalRead(KEY_R1) || digitalRead(KEY_R2)) Keyboard.release(' ');
-        else Keyboard.press(' ');
-
-        if (digitalRead(KEY_L1)) Keyboard.release(KEY_ESC);
-        else Keyboard.press(KEY_ESC);
-        if (digitalRead(KEY_L2)) Keyboard.release(KEY_RETURN);
-        else Keyboard.press(KEY_RETURN);
-
-        if (digitalRead(KEY_LLEFT)) Keyboard.release('x');
-        else Keyboard.press('x');
-        if (digitalRead(KEY_LRIGHT)) Keyboard.release('z');
-        else Keyboard.press('z');
-
-    } else if (!digitalRead(MODE_B) && !digitalRead(MODE_A)) {
-  
-        if (digitalRead(KEY_L1)) Keyboard.release('s');
-        else Keyboard.press('s');
-        if (digitalRead(KEY_LUP)) Keyboard.release(' ');
-        else Keyboard.press(' ');  
-        if (digitalRead(KEY_L2)) Keyboard.release('w');
-        else Keyboard.press('w');
-        if (digitalRead(KEY_LLEFT)) Keyboard.release('d');
-        else Keyboard.press('d');
-        if (digitalRead(KEY_LRIGHT)) Keyboard.release('a');
-        else Keyboard.press('a');
-        
-        if (digitalRead(KEY_R1)) Keyboard.release(';');
-        else Keyboard.press(';');
-        if (digitalRead(KEY_RUP)) Keyboard.release('x');
-        else Keyboard.press('x');
-        if (digitalRead(KEY_R2)) Keyboard.release('\'');
-        else Keyboard.press('\'');
-        if (digitalRead(KEY_RLEFT)) Keyboard.release(',');
-        else Keyboard.press(',');
-        if (digitalRead(KEY_RRIGHT)) Keyboard.release('.');
-        else Keyboard.press('.');
-        
-    } else {
-        
-        
-        
-    }
+    digitalWrite(LLedY, !digitalRead(LWheelBtn));
+    digitalWrite(RLedY, !digitalRead(RWheelBtn));  
 }
